62e8145b1974ccf461aa8df190b00f8d36574719
Patrick Socha <PatrickSocha@users.noreply.github.com>
2022-12-06 22:50:44 +0000

    Now accept multiple lists and their regex

--- blocklist.go
+++ blocklist.go
@@ -13,25 +13,25 @@ import (
 func updateBlockList() {
 	for {
 		log.Println("Getting block list")
-
-		var compRegEx = regexp.MustCompile(`0.0.0.0\s+(?P<url>\S+)`)
-		resp, err := http.Get("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		scanner := bufio.NewScanner(resp.Body)
-
-		// purge the entire list
 		blockListDatabase = make(map[string]interface{})
 
-		// populate the list
-		for scanner.Scan() {
-			v := getParams(compRegEx, scanner.Text())
-			if v != nil {
+		for _, s := range blockListSources {
+			var compRegEx = regexp.MustCompile(s.regex)
+			resp, err := http.Get(s.url)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			scanner := bufio.NewScanner(resp.Body)
 
-				// domains come in as `domain.com.` so we add a `.` to the end so it can be found in the map
-				structuredDomain := *v + "."
-				blockListDatabase[structuredDomain] = struct{}{}
+			// populate the list
+			for scanner.Scan() {
+				v := getParams(compRegEx, scanner.Text())
+				if v != nil {
+
+					// domains come in as `domain.com.` so we add a `.` to the end so it can be found in the map
+					structuredDomain := *v + "."
+					blockListDatabase[structuredDomain] = struct{}{}
+				}
 			}
 		}
 
--- database.go
+++ database.go
@@ -7,8 +7,31 @@ type record struct {
 	ips       []string
 }
 
+type sources struct {
+	regex string
+	url   string
+}
+
 var database = map[string]record{}
 var blockListDatabase = map[string]interface{}{}
+var blockListSources = []sources{
+	{
+		regex: `0.0.0.0\s+(?P<url>\S+)`,
+		url:   "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
+	},
+	{
+		regex: `127.0.0.1\s+(?P<url>\S+)`,
+		url:   "https://adaway.org/hosts.txt",
+	},
+	{
+		regex: `(?P<url>\S+)`,
+		url:   "https://v.firebog.net/hosts/Easyprivacy.txt",
+	},
+	{
+		regex: `0.0.0.0\s+(?P<url>\S+)`,
+		url:   "https://raw.githubusercontent.com/d3ward/toolz/master/src/d3host.txt",
+	},
+}
 var whitelistDatabase = map[string]interface{}{
 	"spclient.wg.spotify.com.": struct{}{},
 }
