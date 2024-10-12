84b978e04cdca3423338178aa0e329a4b7f61200
patricksocha <PatrickSocha@users.noreply.github.com>
2024-10-12 14:02:23 +0200

    clean up

--- dohClient/doh.go
+++ dohClient/doh.go
@@ -21,33 +21,34 @@ func Start(provider ...int) *DohClient {
 
 // QueryAuthority makes DNS over HTTPS request
 func (d *DohClient) QueryAuthority(ctx context.Context, address string, questionQueryType dohDns.Type) []string {
-	rsp, err := d.Doh.Query(ctx, dohDns.Domain(address), questionQueryType)
+	dohResp, err := d.Doh.Query(ctx, dohDns.Domain(address), questionQueryType)
 	if err != nil {
 		log.Printf("%s : doh query failed, retrying: %s", address, err.Error())
-		rsp, err = d.Doh.Query(ctx, dohDns.Domain(address), questionQueryType)
+		dohResp, err = d.Doh.Query(ctx, dohDns.Domain(address), questionQueryType)
 		if err != nil {
 			log.Printf("%s : failed, giving up: %s", address, err.Error())
+			return []string{}
 		}
 	}
-
-	resp := []string{}
-	if rsp == nil {
+	if dohResp == nil {
 		return []string{}
 	}
 
-	for _, answer := range rsp.Answer {
+	queryResp := []string{}
+	for _, answer := range dohResp.Answer {
 		// DNS provider can sometimes return multiple types, only return the one we want
 		// e.g: ipv6.googlg.com returns type 5 (CNAME) and 28 (AAAA) which would break AAAA response
 		responseQueryType, err := models.QueryToDoHType(uint16(answer.Type))
 		if err != nil {
+			log.Printf("error finding doh query type response: %s", err)
 			return []string{}
 		}
 		if responseQueryType != questionQueryType {
 			continue
 		}
 
-		resp = append(resp, answer.Data)
+		queryResp = append(queryResp, answer.Data)
 	}
 
-	return resp
+	return queryResp
 }
